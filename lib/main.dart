fbe4fc6eb26c87ab04e21b6607cbe7525e58b596
Eric <ericcleptonsilva@gmail.com>
2021-02-14 02:03:20 -0300

    Desenhando Layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,79 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Contador de Peossoas",
-    darkTheme: ThemeData.dark(),
-    home: Container(
-      alignment: Alignment.center,
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(0),
-              child: Text(
-                "Peddoas: 0",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      title: "Contador de Peossoas",
+      darkTheme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: Stack(
+        children: <Widget>[
+          Image.asset(
+            "images/restaurante.jpg",
+            fit: BoxFit.cover,
+            height: 1000,
+          ),
+          Stack(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 250,
+                      width: 500,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  "Pode Entrar!: 0",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w300,
-                      fontSize: 25),
-                ))
-          ]),
-      color: Colors.black,
-    ),
-    debugShowCheckedModeBanner: false,
-  ));
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Peddoas: 0",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: TextButton(
+                            child: Text(
+                              "+1",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                              ),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: TextButton(
+                            child: Text(
+                              "-1",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                              ),
+                            ),
+                            onPressed: () {},
+                          ),
+                        )
+                      ],
+                    ),
+                    Text("Pode Entrar!: 0",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 25)),
+                  ]),
+            ],
+          )
+        ],
+      )));
 }
