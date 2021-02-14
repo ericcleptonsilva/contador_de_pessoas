49101c7374a159ea9686b1c13e732eead15aa977
Eric <ericcleptonsilva@gmail.com>
2021-02-14 11:48:19 -0300

    Finalizado o App de Contador de Pessoas

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:contador_de_pessoas/home_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,76 +7,5 @@ void main() {
       title: "Contador de Peossoas",
       darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      home: Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/restaurante.jpg",
-            fit: BoxFit.cover,
-            height: 1000,
-          ),
-          Stack(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(15),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      height: 250,
-                      width: 500,
-                      color: Colors.black,
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "Peddoas: 0",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: TextButton(
-                            child: Text(
-                              "+1",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 40,
-                              ),
-                            ),
-                            onPressed: () {},
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: TextButton(
-                            child: Text(
-                              "-1",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 40,
-                              ),
-                            ),
-                            onPressed: () {},
-                          ),
-                        )
-                      ],
-                    ),
-                    Text("Pode Entrar!: 0",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w300,
-                            fontSize: 25)),
-                  ]),
-            ],
-          )
-        ],
-      )));
+      home: HomePage()));
 }
